8b8e3a3090783c37071b83b9a306035ea5815092
LaloHaxSkids <66586111+LaloHaxSkids@users.noreply.github.com>
2021-05-14 18:46:15 -0700

    Update bot_pm.py

--- userge/plugins/bot/bot_pm.py
+++ userge/plugins/bot/bot_pm.py
@@ -180,13 +180,15 @@ if userge.has_bot:
             start_msg, btns = default_owner_start(from_user)
         else:
             start_msg = f"""
+Hello 👋 {from_user.fname},
+Nice To Meet You !, I'm <b>{bot_.fname}</b> A Bot.
 
-       
+        <b><i>Powered by</i> [USERGE-X](https://t.me/x_xtests)
 
 My Master is : {owner_.flname}</b>
 """
             if Config.BOT_FORWARDS:
-                start_msg += ""
+                start_msg += "<b>\n📌 NOTE:</b>\nYou can 📨 <b>Send Message</b> here to contact my <b>Master.</b>"
             contact_url = (
                 f"https://t.me/{owner_.uname}"
                 if owner_.uname
@@ -194,8 +196,8 @@ My Master is : {owner_.flname}</b>
             )
             btns = [
                 [
-                    InlineKeyboardButton("),
-                    InlineKeyboardButton("),
+                    InlineKeyboardButton("👤  CONTACT", url=contact_url),
+                    InlineKeyboardButton("⭐️  REPO", url=Config.UPSTREAM_REPO),
                 ]
             ]
         try:
